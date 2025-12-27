0b6e6f92a6874baf99c828c01c76b4b1f84c6b79
Devashish Rathod <rathod.devashish09@gmail.com>
2025-12-28 00:00:45 +0530

    server category addition

--- server/controllers/categories/createCategory.js
+++ server/controllers/categories/createCategory.js
@@ -1,10 +1,15 @@
-const { asyncWrapper, sendSuccess, throwError } = require("../../utils");
+const {
+  asyncWrapper,
+  sendSuccess,
+  throwError,
+  cleanJoiError,
+} = require("../../utils");
 const { createCategory } = require("../../services/categories");
 const { validateCreateCategory } = require("../../validator/categories");
 
 exports.createCategory = asyncWrapper(async (req, res) => {
   const { error } = validateCreateCategory(req.body);
-  if (error) throwError(422, error.details.map((d) => d.message).join(", "));
+  if (error) throwError(422, cleanJoiError(error));
   const image = req.files?.image;
   const category = await createCategory(req.body, image);
   return sendSuccess(res, 201, "Category created", category);
